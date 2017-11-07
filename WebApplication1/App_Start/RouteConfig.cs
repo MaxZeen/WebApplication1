bc8eefdf1bf8b3ef9496cc4b5a41ce03062e9355
MaxZeen <zyun99@163.com>
2017-11-07 10:00:36 +0800

    update with redis demo

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -22,7 +22,7 @@ namespace WebApplication1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Test", action = "NgTable", id = UrlParameter.Optional }
+                defaults: new { controller = "Test", action = "RedisStudy", id = UrlParameter.Optional }
                 );
         }
     }
--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ServiceStack.Redis;
 
 namespace WebApplication1.Views.Test
 {
@@ -35,6 +36,31 @@ namespace WebApplication1.Views.Test
             var items = da.GetLicensensList();
           //  var jsonstr= JsonConvert.SerializeObject(items);
             return Json(new { data= items.ToArray() }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult RedisStudy()
+        {
+     
+
+            var client = new RedisClient("127.0.0.1", 6379);
+            client.Set<int>("pwd", 1111);
+            int pwd = client.Get<int>("pwd");
+
+            //UserInfo userInfo = new UserInfo() { UserName = "zhangsan", UserPwd = "1111" };//</span>(底层使用json序列化 )  
+            //client.Set<UserInfo>("userInfo", userInfo);
+            //UserInfo user = client.Get<UserInfo>("userInfo");
+            //Console.WriteLine(user.UserName);
+
+            //List<UserInfo> list = new List<UserInfo>() { new UserInfo() { UserName = "lisi", UserPwd = "111" }, new UserInfo() { UserName = "wangwu", UserPwd = "123" } };
+            //client.Set<List<UserInfo>>("list", list);
+            //List<UserInfo> userInfoList = client.Get<List<UserInfo>>("list");
+
+            //foreach (UserInfo userInfo in userInfoList)
+            //{
+            //    Console.WriteLine(userInfo.UserName);
+            //}
+
+            return View();
         }
     }
 }
